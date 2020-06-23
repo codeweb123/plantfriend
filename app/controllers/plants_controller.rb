934d3bb12c0802a2b58aa5459e7f9cc629fbad75
codeweb123 <jiyonj@hotmail.com>
2020-06-23 00:02:51 -0400

    refactored and placed check for signin duplicates

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -16,6 +16,7 @@ class PlantsController < ApplicationController
     #(NEW)Show form to create a new instance of a plant.
       get '/plants/new' do
         if logged_in?
+          @user = current_user
           erb :'/plants/new'
         else
           flash[:message] = "Looks like you weren't logged in yet. Please log in below."
@@ -25,7 +26,7 @@ class PlantsController < ApplicationController
 
     #(CREATE)Process the form and create a plant object in DB.
       post '/plants' do
-        @user = current_user
+          @user = current_user
         if params[:name] == "" || 
            params[:num_water_day] == "" || 
            params[:num_fert_day] == "" || 
@@ -33,11 +34,13 @@ class PlantsController < ApplicationController
            flash[:message] = "Oops! PLants must have a name, water day, fertilizer day and prune day. Please try again."
            redirect to '/plants/new'
         else
-          @plant = Plant.create(
+          @plant = Plant.new(
             :name => params[:name],
             :num_water_day => params[:num_water_day],
             :num_fert_day => params[:num_fert_day],
-            :num_prune_day => params[:num_prune_day]
+            :num_prune_day => params[:num_prune_day],
+            :location_id => params[:location_id],
+            :user_id => params[:user_id]
           )
           @plant.user_id = current_user.id
           @plant.save
@@ -49,41 +52,48 @@ class PlantsController < ApplicationController
     #(SHOW)READ:GET action show that view page to show that particular plant
       get '/plants/:id' do
         #id = params[:id]
-        @plant = Plant.find_by(id: params[:id])
-        erb :'/plants/show'
+          @plant = Plant.find_by(id: params[:id])
+          erb :'/plants/show'
       end
 
       #(EDIT) / read
       get '/plants/:id/edit' do
-        #if is_logged_in? && authorized?(@plant)
-          @plant = Plant.find_by(id: params[:id])
-          current_user
-          erb :'/plants/edit'
-      end
+        if logged_in?
+            @plant = Plant.find_by(id: params[:id])
+        if @plant.user == current_user
+            @user = current_user
+            erb :'/plants/edit'
+        end
 
     #(UPDATE) use middleware (Rack::MethodOverride) to send put/patch/delete requests 
       put '/plants/:id' do
-        @plant = Plant.find_by(id: params[:id])
-        @plant.update(
-          :name => params[:name],
-          :num_water_day => params[:num_water_day],
-          :num_fert_day => params[:num_fert_day],
-          :num_prune_day => params[:num_prune_day],
-          :location_id => params[:location_id]
-        )
-        redirect "/plants/#{@plant.id}"
+        if  @plant = Plant.find_by(id: params[:id])
+            @plant.update(
+              :name => params[:name],
+              :num_water_day => params[:num_water_day],
+              :num_fert_day => params[:num_fert_day],
+              :num_prune_day => params[:num_prune_day],
+              :location_id => params[:location_id],
+              :user_id => params[:user_id]
+            )
+            @plant.user = current_user
+            @plant.save
+        end
+          redirect "/plants/#{@plant.id}"
       end
 
     #(DESTROY) a plant
       delete '/plants/:id' do
-         if @plant = Plant.find_by(id: params[:id])
-          current_user
-          @plant.destroy
-          flash[:message] = "The plant was deleted."
-          redirect to '/plants/index'
+         if logged_in?
+            @plant = Plant.find_by(id: params[:id])
+            @plant.user == current_user
+            @user = current_user
+            @plant.destroy
+            flash[:message] = "The plant was deleted."
+            redirect to '/plants/index'
         else
-          flash[:message] = "Looks like you weren't logged in yet. Please log in below."
-          redirect to '/login'
+            flash[:message] = "Looks like you weren't logged in yet. Please log in below."
+            redirect to '/login'
         end
       end
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,48 +3,56 @@ class UsersController < ApplicationController
     #show the form (prefix the route)
       get '/users/signup' do #route
           redirect to '/users/signup'
-        end
-      #Processing form and create new object
+      end
+    #Processing form and create new object
       post '/users/signup' do
-        if  params[:username] == "" || #check if we have bad data
+          @user = User.new(
+          :username => params[:username], 
+          :email => params[:email], 
+          :password => params[:password]
+        )  
+          if @user.save
+            session[:user_id] = @user.id #store it inside an instance variable, (username:) is the name of the column and insert the params[:username] from the params  
+            erb :'users/login'
+          elsif
+            User.find_by(username: params[:username])
+            flash[:message] = "That username already exist. Please choose another one."
+            redirect to '/users/signup'
+          elsif
+            params[:username] == "" || #check we have bad data
             params[:email] == "" || 
             params[:password] == ""
-          flash[:message] = "In order to sign up account, you must have a username, email & password. Please try again."
-          redirect to '/users/signup'
-        elsif
-          logged_in?
+            flash[:message] = "In order to sign up account, you must have a username, email & password. Please try again."
+            redirect to '/users/signup'
+          else
+            logged_in?
             flash[:message] = "You are already logged in."
             redirect to '/users/login' 
-        else  
-          @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])  
-           if @user.save
-            session[:user_id] = @user.id #store it inside an instance variable, (username:) is the name of the column and insert the params[:username] from the params
-          #binding.pry  
-          erb :'users/login'
-        end
+          end
       end
+
       #Login
       get '/users/login' do
           erb :'/users/login'
-        end
+      end
     #process form and create a new user object
       post '/users/login' do
-        @user = User.find_by(username: params[:username])
+          @user = User.find_by(username: params[:username])
         #binding.pry
-        if @user && @user.authenticate(params[:password]) #def authenticate(string)-compares the string to the encrypted (salts & encrypts) password and returns either false or the instance of the user again(true) given by Bcrypt.
-          session[:user_id] = @user.id 
-          #binding.pry  #logs user in and sets session
-          erb :'/locations/new' 
+          if @user && @user.authenticate(params[:password]) 
+#def authenticate(string)-compares the string to the encrypted (salts & encrypts) password 
+#and returns either false or the instance of the user again(true) given by Bcrypt.
+            session[:user_id] = @user.id #logs user in and sets session
+            erb :'/locations/new' 
           #direct them to add a location
-        else
-          flash[:message] = "Your username or password were not correct. Please try again."
-          redirect to "/users/login" 
-        end
+          else
+            flash[:message] = "Your username or password were not correct. Please try again."
+            redirect to "/users/login" 
+          end
       end
-      
       # Log out
       get '/users/logout' do
           session.clear
           redirect to '/'
+        end
       end
-    end
